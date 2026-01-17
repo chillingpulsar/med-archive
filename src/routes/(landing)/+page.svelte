7db512a0a1ea61506310da787041b32a5b4ec68e
Mike John Eviota <mikejohneviota@Mikes-Mac-mini.local>
2026-01-17 13:47:33 +0800

    add login structure
    wips

---
+++ src/routes/(landing)/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Login from './(components)/login/login.svelte';
+	import Register from './(components)/register/register.svelte';
+	import ForgotPwd from './(components)/forgot-pwd/forgot-pwd.svelte';
+	import { page } from '$app/state';
+
+	const params = $derived(page.url.searchParams.get('q'));
+</script>
+
+<main class="grid min-h-screen md:grid-cols-2">
+	<secttion>
+		<h1>MedArchive</h1>
+		<p>Your Personal Medical Archive</p>
+	</secttion>
+
+	{#if params === 'register'}/>
+		<Register />
+	{:else if params === 'forgot-pwd'}
+		<ForgotPwd />
+	{:else}
+		<Login />
+	{/if}
+</main>
---
+++ src/routes/(landing)/schema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod/v4";
+
+export const loginSchema = z.object({
+    email: z.email("Must enter a valid email address"),
+    pwd: z.string().min(1, "Must enter a password")
+});
+
+export type LoginSchema = z.infer<typeof loginSchema>;
