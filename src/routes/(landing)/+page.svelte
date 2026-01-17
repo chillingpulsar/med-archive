d977a5d563f21fa6cb16b150181abe91cdd876a3
Mike John Eviota <mikejohneviota@Mikes-Mac-mini.local>
2026-01-17 14:14:38 +0800

    add login ui

--- src/routes/(landing)/+page.svelte
+++ src/routes/(landing)/+page.svelte
@@ -4,20 +4,28 @@
 	import ForgotPwd from './(components)/forgot-pwd/forgot-pwd.svelte';
 	import { page } from '$app/state';
 
+	const { data } = $props();
+
 	const params = $derived(page.url.searchParams.get('q'));
 </script>
 
-<main class="grid min-h-screen md:grid-cols-2">
-	<secttion>
-		<h1>MedArchive</h1>
-		<p>Your Personal Medical Archive</p>
+<main class="container grid min-h-screen p-4 md:grid-cols-2">
+	<secttion class="flex flex-col items-center justify-center gap-2">
+		<div class="mt-auto grid max-w-sm gap-2.5 text-center md:mt-0">
+			<h1 class="text-4xl font-bold">MedArchive</h1>
+			<p class="text-sm text-muted-foreground">
+				Track your health and medications with ease in one place with AI-powered insights
+			</p>
+
+			<div class="mt-2 hidden h-100 w-full rounded-lg bg-secondary md:block"></div>
+		</div>
 	</secttion>
 
-	{#if params === 'register'}/>
+	{#if params === 'register'}
 		<Register />
 	{:else if params === 'forgot-pwd'}
 		<ForgotPwd />
 	{:else}
-		<Login />
+		<Login loginForm={data.loginForm} />
 	{/if}
 </main>
--- src/routes/(landing)/schema.ts
+++ src/routes/(landing)/schema.ts
@@ -1,8 +1,8 @@
-import { z } from "zod/v4";
+import { z } from 'zod/v4';
 
 export const loginSchema = z.object({
-    email: z.email("Must enter a valid email address"),
-    pwd: z.string().min(1, "Must enter a password")
+	email: z.email('Must enter a valid email address'),
+	pwd: z.string().min(1, 'Must enter a password')
 });
 
 export type LoginSchema = z.infer<typeof loginSchema>;
