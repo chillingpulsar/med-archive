a98103271190c75f974d71d554c32f41f22bb11d
Mike John Eviota <mikejohneviota@Mikes-Mac-mini.local>
2026-01-24 15:53:09 +0800

    update login ui

--- src/routes/(landing)/+page.svelte
+++ src/routes/(landing)/+page.svelte
@@ -9,15 +9,15 @@
 	const params = $derived(page.url.searchParams.get('q'));
 </script>
 
-<main class="container grid min-h-screen p-4 md:grid-cols-2">
-	<secttion class="flex flex-col items-center justify-center gap-2">
+<main class="grid min-h-screen md:grid-cols-2">
+	<secttion class="flex flex-col items-center justify-center gap-2 bg-secondary">
 		<div class="mt-auto grid max-w-sm gap-2.5 text-center md:mt-0">
 			<h1 class="text-4xl font-bold">MedArchive</h1>
 			<p class="text-sm text-muted-foreground">
 				Track your health and medications with ease in one place with AI-powered insights
 			</p>
 
-			<div class="mt-2 hidden h-100 w-full rounded-lg bg-secondary md:block"></div>
+			<div class="mt-2 hidden h-100 w-full rounded-lg bg-gray-500 md:block"></div>
 		</div>
 	</secttion>
 
