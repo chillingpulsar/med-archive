904db48cd372f56a8b8a31564441ce550c4e438d
Mike John Eviota <mikejohneviota@Mikes-Mac-mini.local>
2026-01-24 17:40:35 +0800

    add dashboard ui sample

---
+++ src/routes/(authenticated)/dashboard/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+</script>
+
+<main class="flex flex-col gap-4">
+	<section>
+		<div class="flex w-fit flex-col gap-1 border p-4">
+			<p>Total Records: 10</p>
+		</div>
+	</section>
+
+	<!--Patient Information-->
+	<section class="flex flex-col gap-2 border p-4">
+		<h1 class="text-2xl font-bold">Patient Information</h1>
+		<div class="flex flex-col gap-1">
+			<p>Name: John Doe</p>
+			<p>Age: 30</p>
+			<p>Gender: Male</p>
+			<p>Age / Date of Birth: 1990-01-01</p>
+			<p>Blood Type: A+</p>
+		</div>
+	</section>
+
+	<section class="flex flex-col gap-2 border p-4">
+		<!--Generate by AI, based on the patient information and records-->
+		<h1 class="text-2xl font-bold">Health Summary</h1>
+		<p class="text-base text-muted-foreground">
+			Based on your records, Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo provident
+			velit laborum tenetur fuga, harum dolor nulla possimus at saepe deserunt itaque eius
+			accusantium culpa ut, sed vel veniam beatae.
+		</p>
+	</section>
+</main>
