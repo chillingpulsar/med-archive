afeec3fd09a3eee8f4bd9f7bb44b0e821c4f8576
Mike John Eviota <mikejohneviota@Mikes-Mac-mini.local>
2026-03-20 18:57:45 +0800

    improve format

--- src/routes/(authenticated)/records/+page.server.ts
+++ src/routes/(authenticated)/records/+page.server.ts
@@ -1,23 +1,23 @@
-import type { Actions, PageServerLoad } from "./$types";
-import { addRecordSchema } from "./schema";
-import { zod4 } from "sveltekit-superforms/adapters";
-import { superValidate } from "sveltekit-superforms";
-import { fail } from "@sveltejs/kit";
+import type { Actions, PageServerLoad } from './$types';
+import { addRecordSchema } from './schema';
+import { zod4 } from 'sveltekit-superforms/adapters';
+import { superValidate } from 'sveltekit-superforms';
+import { fail } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async () => {
-    return {
-        addRecordForm: await superValidate(zod4(addRecordSchema))
-    }
+	return {
+		addRecordForm: await superValidate(zod4(addRecordSchema))
+	};
 };
 
 export const actions: Actions = {
-    addRecordEvent: async ({ request }) => {
-        const form = await superValidate(request, zod4(addRecordSchema));
+	addRecordEvent: async ({ request }) => {
+		const form = await superValidate(request, zod4(addRecordSchema));
 
-        if (!form.valid) return fail(400, { form });
+		if (!form.valid) return fail(400, { form });
 
-        console.log(form.data);
+		console.log(form.data);
 
-        return { form, msg: 'Record added successfully' };
-    }
+		return { form, msg: 'Record added successfully' };
+	}
 };
